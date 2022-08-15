cb98978f7c0fafd9653deda7bd33cdbecbd9bba7
StephenTurk90 <stephenjturk@gmail.com>
2022-08-15 14:25:12 -0400

    add function to read a file

--- mystery_word.py
+++ mystery_word.py
@@ -2,5 +2,26 @@ def play_game():
     pass
 
 
+# create blank board based on the letter
+
 if __name__ == "__main__":
     play_game()
+
+
+
+
+file = Path(args.file)
+    import argparse
+    from pathlib import Path
+
+    parser = argparse.ArgumentParser(
+        description="Let's play Mystery Word")
+    parser.add_argument('file', help='file to read')
+    args = parser.parse_args()
+
+if file.is_file():
+        mystery_word(file)
+    else:
+        print(f"{file} does not exist!")
+        exit(1)
+# this bring up an error message if the txt file does not exist
