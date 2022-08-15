281180edb37d3f9d4ffb651be1384519085f9b14
StephenTurk90 <stephenjturk@gmail.com>
2022-08-15 16:23:33 -0400

    changed the allowed guesses from 26 to 8.

--- mystery_word.py
+++ mystery_word.py
@@ -1,55 +1,35 @@
-# # import random
-# """Downloads random number generator"""
+import random
 
-correct_list = []
+# with open('text-word.txt', 'r') as file:
+#     correct_word = file.read().replace('\n', '')
+
+correct_word = (random.choice(open("words.txt", "r").read().split()))
+
+right_word = []
 wrong_list = []
+# 2 empty list
 
-
-# def play_game():
-#     pass
-#
-#
-#
-# 
-#
-#
-#
-#
-#
 
 def guess_letters(word):
     letters_in_correct_word = ["_" for letter in word]
-    tries = 8
+    tries = 26
     while tries > 0:
-        guess_letter = input("Guess a letter: ")
-        if guess not in correct_word:
+        guess = input("Guess a letter: ")
+        if guess not in word:
             tries -= 1
             print(f"Wrong, have {tries} more tries")
+            if tries == 0:
+                print("You Lose!")
+                print("Do you want to play again?")
         else:
             for i in range(len(letters_in_correct_word)):
-                if guess == letters_in_correct_word[i]:
+                if guess == word[i]:
                     letters_in_correct_word[i] = guess
             print(letters_in_correct_word)
 
+
 guess_letters(correct_word)
-# create blank board based on the letter
 
-# if __name__ == "__main__":
-#     play_game()
 
-# if __name__ == "__main__":
-#     import argparse
-#     from pathlib import Path
-
-#     parser = argparse.ArgumentParser(
-#         description='Get the word frequency in a text file.')
-#     parser.add_argument('file', help='file to read')
-#     args = parser.parse_args()
-
-#     file = Path(args.file)
-#     if file.is_file():
-#         mystery_word(file)
-#     else:
-#         print(f"{file} does not exist!")
-#         exit(1)
-# this bring up an error message if the txt file does not exist
+if __name__ == "__main__":
+    guess_letters()
