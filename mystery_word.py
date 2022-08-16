f6fffd591cf601e59b89d409706b6787bee4671e
StephenTurk90 <stephenjturk@gmail.com>
2022-08-16 16:29:42 -0400

    format code to be more python by reducing line lenghths

--- mystery_word.py
+++ mystery_word.py
@@ -22,10 +22,13 @@ You have {tries} tries.\n")
             print("Invalid input.")
             # More than one letter is invalid, let them try again
             # need to make it so only one letter can be used
-            print(f"Please Try Again! The length of the word is {length_of_word} letters.\n\
+            print(f"Please Try Again! \
+The length of the word is\
+{length_of_word} letters.\n\
 {' '.join(letters_in_correct_word)}\n\
-You have {tries} tries.")
-            print(f"Your wrong choices so far are: {', '.join(wrong_choices)}\n")
+You still have {tries} tries.")
+            print(f"Your wrong choices so far are: \
+{', '.join(wrong_choices)}\n")
         else:
             # need to make it so words used previously cannot be used again?
             if guess not in correct_word:
@@ -36,9 +39,11 @@ You have {tries} tries.")
                     print(f"Wrong! You have {tries} more tries.")
                     wrong_choices.append(guess)
                     print(f"Your word is {' '.join(letters_in_correct_word)}")
-                    print(f"Your wrong choices so far are: {', '.join(wrong_choices)}\n")
+                    print(f"Your wrong choices so far are: \
+{', '.join(wrong_choices)}\n")
                     if tries == 0:
-                        print(f'You Lose! The correct answer was: "{correct_word}" ')
+                        print(f'You Lose! The correct answer was: "\
+{correct_word}" ')
             else:
                 if guess in letters_in_correct_word:
                     print("Letter has already been used")
@@ -47,7 +52,8 @@ You have {tries} tries.")
                         if guess == correct_word[i]:
                             letters_in_correct_word[i] = guess
                     print(f"Correct!\n{' '.join(letters_in_correct_word)}")
-                    print(f"Your wrong choices so far are: {', '.join(wrong_choices)}\n")
+                    print(f"Your wrong choices so far are: \
+{', '.join(wrong_choices)}\n")
                     if correct_word == "".join(letters_in_correct_word):
                         print("You win!")
 
