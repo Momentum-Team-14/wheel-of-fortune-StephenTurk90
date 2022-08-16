35ab21923b240490aae4269c06b7291574f022bc
StephenTurk90 <stephenjturk@gmail.com>
2022-08-16 16:09:53 -0400

    attempted a loop to replay the game, made invalid inputs register as invalid inputs.

--- mystery_word.py
+++ mystery_word.py
@@ -11,36 +11,50 @@ def play_game():
     # debug end
     wrong_choices = []
     tries = 8
-    print(f"The length of the word is {length_of_word} letters.\n\
+    print(f"Let's Begin! The length of the word is {length_of_word} letters.\n\
 {' '.join(letters_in_correct_word)}\n\
 You have {tries} tries.\n")
     # This is the step for counting word length
     while tries > 0:
-        guess = input("Guess a letter: ")
-        # This letter can be upper or lower case and it should not matter
-        # need to make it so cap or lower case both work
-        # More than one letter is invalid, let them try again
-        # need to make it so only one letter can be used
-        # need to make it so words used previously cannot be used again?
-        if guess not in correct_word:
-            tries -= 1
-            print(f"Wrong! You have {tries} more tries.")
-            wrong_choices.append(guess)
-            print(f"Your word is {' '.join(letters_in_correct_word)}")
+        guess = input("Guess a letter: ").lower()
+        # this code allows the capital or lower to work
+        if len(guess) >= 2:
+            print("Invalid input.")
+            # More than one letter is invalid, let them try again
+            # need to make it so only one letter can be used
+            print(f"Please Try Again! The length of the word is {length_of_word} letters.\n\
+{' '.join(letters_in_correct_word)}\n\
+You have {tries} tries.")
             print(f"Your wrong choices so far are: {', '.join(wrong_choices)}\n")
-            if tries == 0:
-                print(f'You Lose! The correct answer was: "{correct_word}" ')
-                print("Do you want to play again?\n")
         else:
-            for i in range(len(letters_in_correct_word)):
-                if guess == correct_word[i]:
-                    letters_in_correct_word[i] = guess
-            print(f"Correct!\n{' '.join(letters_in_correct_word)}")
-            print(f"Your wrong choices so far are: {', '.join(wrong_choices)}\n")
-            if correct_word == "".join(letters_in_correct_word):
-                print("You win")
-                return
-                # need to make a loop
+            # need to make it so words used previously cannot be used again?
+            if guess not in correct_word:
+                if guess in wrong_choices:
+                    print("Letter has already been used")
+                else:
+                    tries -= 1
+                    print(f"Wrong! You have {tries} more tries.")
+                    wrong_choices.append(guess)
+                    print(f"Your word is {' '.join(letters_in_correct_word)}")
+                    print(f"Your wrong choices so far are: {', '.join(wrong_choices)}\n")
+                    if tries == 0:
+                        print(f'You Lose! The correct answer was: "{correct_word}" ')
+            else:
+                if guess in letters_in_correct_word:
+                    print("Letter has already been used")
+                else:
+                    for i in range(len(letters_in_correct_word)):
+                        if guess == correct_word[i]:
+                            letters_in_correct_word[i] = guess
+                    print(f"Correct!\n{' '.join(letters_in_correct_word)}")
+                    print(f"Your wrong choices so far are: {', '.join(wrong_choices)}\n")
+                    if correct_word == "".join(letters_in_correct_word):
+                        print("You win!")
+
+    play = input("Do you want play again? (y/n)").lower()
+    if (play == 'y'):
+        print(' ')
+        play_game()
 
 
 if __name__ == "__main__":
