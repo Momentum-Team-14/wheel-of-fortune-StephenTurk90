2c550e2b09795a743097c5a38d63b3742d7815cb
StephenTurk90 <stephenjturk@gmail.com>
2022-08-16 10:50:24 -0400

    added additional lines of text to begin the game, created a list of wrong words. Made it show all words can show up.

--- mystery_word.py
+++ mystery_word.py
@@ -6,32 +6,34 @@ import random
 correct_word = (random.choice(open("words.txt", "r").read().split()))
 # this allows the words in the list to be deciphered
 
-right_list = []
-wrong_list = []
-# 2 empty list
-# need to define what's a right word
-# need to define what's a wrong word
-
 
 def guess_letters(word):
     letters_in_correct_word = ["_" for letter in word]
     length_of_word = len(word)
+    wrong_choices = []
     tries = 8
-    print(f"The length of the word is {length_of_word}. You have {tries} tries")
+    print(f"The length of the word is {length_of_word} letters.\n\
+{letters_in_correct_word}\n\
+You have {tries} tries.")
     # This is the step for counting word length
     while tries > 0:
         guess = input("Guess a letter: ")
         if guess not in word:
             tries -= 1
             print(f"Wrong, have {tries} more tries")
+            wrong_choices.append(guess)
+            print(f"Your word is {letters_in_correct_word}")
+            print(f"Your wrong choices so far are: {wrong_choices}")
             if tries == 0:
                 print("You Lose!")
-                print("Do you want to play again?")
+                print("Do you want to play again?\n \
+Choose your first letter to begin.")
         else:
             for i in range(len(letters_in_correct_word)):
                 if guess == word[i]:
                     letters_in_correct_word[i] = guess
-            print(letters_in_correct_word)
+            print(f"Correct!\n{letters_in_correct_word}")
+            print(f"Your wrong choices so far are: {wrong_choices}")
 
 
 guess_letters(correct_word)
